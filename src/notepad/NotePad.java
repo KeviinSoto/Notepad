0519f58315fa7038865977c1ff6e18edf55e6b75
Kevin Soto <kevin.soto08@gmail.com>
2014-08-15 11:54:43 -0400

    notepad

---
+++ src/notepad/NotePad.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepad;
+
+/**
+ *
+ * @author Kevin Emil Tejeda Soto
+ * Matricula: 30455
+ * Usuario: KevinSoto
+ */
+public class NotePad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
